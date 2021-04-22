129ad9f9bcf391ae2cd1aa93438cf43032dab9ed
z505 <github@z505.com>
2021-04-22 17:15:57 -0600

    Added integer pair list

--- BigLists.pas
+++ BigLists.pas
@@ -8,26 +8,52 @@ uses
 type
   //TIntegerSize = (szByte, szInt16, szInt32, szInt64);
 
-  // stores more than millions of numbers if required by using a file
-  TBigIntegerList = class
+  TBigList = class
     constructor Create(FilePath: string);
     destructor Free;
+    procedure SetCount(count: int64);
+    class procedure Error(Msg: PResStringRec; Data: NativeInt);
+  private
+    FCount: int64;
+    FFile: TFileStream;
+  public
+    property Count: Int64 read FCount write SetCount;
+  end;
+
+  // stores one ore more up to millions of numbers in a list by using a file
+  TBigIntegerList = class(TBigList)
     procedure Add(number: int64);
     procedure AddStr(number: string);
     private
-      FCount: int64;
-      FFile: TFileStream;
       // function GetIntSize
-      procedure SetCount(count: int64);
       function GetValue(const Index: int64): int64;
       procedure SetValue(const Index, value: int64);
-      class procedure Error(Msg: PResStringRec; Data: NativeInt);
-
     public
       //property IntegerType: TIntegerSize; read GetIntSize write SetIntSize;
-      property Count: Int64 read FCount write SetCount;
       property Items[const Index: int64]: int64 read GetValue write SetValue;
       // property ItemsStr[const Number: Int64]: string read GetStrValue write SetStrValue;
+
+  end;
+
+  type
+    TIntegerPair = record
+      First: int64;
+      Second: int64;
+    end;
+
+  // like the BigIntegerList but stores a pair of numbers (two) per each item
+  TBigIntegerPairList = class(TBigList)
+    procedure Add(First, Second: int64);
+    procedure AddStr(First, Second: string);
+  private
+    // function GetIntSize
+    function GetValues(const Index: int64): TIntegerPair;
+    procedure SetValues(const Index: int64; value: TIntegerPair);
+
+  public
+    //property IntegerType: TIntegerSize; read GetIntSize write SetIntSize;
+    property Items[const Index: int64]: TIntegerPair read GetValues write SetValues;
+    // property ItemsStr[const Number: Int64]: string read GetStrValue write SetStrValue;
 
   end;
 
@@ -49,7 +75,7 @@ begin
     FileClose(h);
 end;
 
-constructor TBigIntegerList.Create(FilePath: string);
+constructor TBigList.Create(FilePath: string);
 begin
   MakeFile(FilePath);
   FFile := TFileStream.create(FilePath, fmOpenReadWrite);
@@ -57,7 +83,7 @@ begin
   FCount := 0;
 end;
 
-destructor TBigIntegerList.Free;
+destructor TBigList.Free;
 begin
   FFile.free;
   FFile := nil;
@@ -75,7 +101,7 @@ begin
   Add(StrToInt(number));
 end;
 
-procedure TBigIntegerList.SetCount(count: Int64);
+procedure TBigList.SetCount(count: Int64);
 begin
   FCount := count;
 end;
@@ -89,7 +115,7 @@ begin
   if FilePos > FFile.Size then begin
     Error(@reInvalidListItem, Index);
   end else begin
-    FFile.Position := SizeOf(index)*index;
+    FFile.Position := FilePos;
     FFile.Read(buf, SizeOf(buf));
     result := buf;
   end;
@@ -103,11 +129,48 @@ begin
   FFile.write(value, SizeOf(value));
 end;
 
-class procedure TBigIntegerList.Error(Msg: PResStringRec; Data: NativeInt);
+class procedure TBigList.Error(Msg: PResStringRec; Data: NativeInt);
 begin
   raise EListError.CreateFmt(LoadResString(Msg), [Data]) at ReturnAddress;
 end;
 
+procedure TBigIntegerPairList.Add(First, Second: int64);
+begin
+  FFile.Position := SizeOf(First)*Count + SizeOf(Second)*Count;
+  inc(FCount);
+  FFile.Write(First, SizeOf(First));
+  FFile.Write(Second, SizeOf(Second));
+end;
+
+procedure TBigIntegerPairList.AddStr(First, Second: string);
+begin
+  Add(StrToInt(First), StrToInt(Second));
+end;
+
+function TBigIntegerPairList.GetValues(const Index: int64): TIntegerPair;
+var
+  buf: array [1..2] of int64;
+  FilePos: int64;
+begin
+  FilePos := (SizeOf(index)*index)*2;
+  if FilePos > FFile.Size then begin
+    Error(@reInvalidListItem, Index);
+  end else begin
+    FFile.Position := FilePos;
+    FFile.Read(buf, SizeOf(buf));
+    result.First := buf[1];
+    result.Second := buf[2];
+  end;
+end;
+
+procedure TBigIntegerPairList.SetValues(const index: int64; value: TIntegerPair);
+var
+  buf: int64;
+begin
+  FFile.Position := (SizeOf(index)*index)*2;
+  FFile.write(value.First, SizeOf(value.First));
+  FFile.write(value.Second, SizeOf(value.Second));
+end;
 
 
 end.
